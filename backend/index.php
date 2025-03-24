16ea28748aed82e96432a0c60c4a16aee6e34330
Aaron Weidele <aweidele@gmail.com>
2025-03-24 16:07:20 -0400

    initial commit for 2025 version

---
+++ backend/index.php
@@ -0,0 +1,33 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=utf-8');
+
+$artwork = [
+
+  [
+    "title" => "Praesent ac massa at ligula laoreet iaculis.",
+    "id" => "artwork-1"
+  ],
+
+  [
+    "title" => "Aliquam lobortis.",
+    "id" => "artwork-2"
+  ],
+
+  [
+    "title" => "Nullam dictum felis eu pede mollis pretium.",
+    "id" => "artwork-3"
+  ],
+
+  [
+    "title" => "Fusce risus nisl, viverra et, tempor et, pretium in, sapien.",
+    "id" => "artwork-4"
+  ],
+
+  [
+    "title" => "Pellentesque posuere.",
+    "id" => "artwork-5"
+  ]
+];
+
+echo json_encode($artwork);
