9fb41413b91c8a8ad2ab8c72805dd38f1dd63886
Aaron Weidele <aweidele@gmail.com>
2025-04-23 14:17:02 -0400

    styling login

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,40 +1,69 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 export const Login = () => {
+  const [pin, setPin] = useState(Array(6).fill(""));
   const [token, setToken] = useState(localStorage.getItem("token"));
-  const [pin, setPin] = useState("");
   const [error, setError] = useState("");
 
-  const handleLogin = async () => {
-    const response = await fetch("https://rw-api.lndo.site/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ pin }),
-    });
+  const inputRefs = useRef([]);
 
-    const data = await response.json();
-    console.log(data);
-    if (data.success) {
-      localStorage.setItem("token", data.token);
-      setToken(data.token);
-      setError("");
-    } else {
-      setError("Invalid PIN");
+  useEffect(() => {
+    inputRefs.current[0]?.focus();
+  }, []);
+
+  const handleChange = (index, value) => {
+    console.log(index, inputRefs.current);
+    const newPin = [...pin];
+    newPin[index] = value;
+    setPin(newPin);
+
+    if (value && index < pin.length - 1) {
+      inputRefs.current[index + 1]?.focus();
     }
   };
+
+  const handleKeyDown = (index, e) => {
+    if (e.key === "Backspace" && !pin[index] && index > 0) {
+      inputRefs.current[index - 1]?.focus();
+    }
+  };
+
+  console.log(pin);
+  // const [token, setToken] = useState(localStorage.getItem("token"));
+  // const [pin, setPin] = useState("");
+  // const [error, setError] = useState("");
+
+  // const handleLogin = async () => {
+  //   const response = await fetch("https://rw-api.lndo.site/login", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ pin }),
+  //   });
+
+  //   const data = await response.json();
+  //   console.log(data);
+  //   if (data.success) {
+  //     localStorage.setItem("token", data.token);
+  //     setToken(data.token);
+  //     setError("");
+  //   } else {
+  //     setError("Invalid PIN");
+  //   }
+  // };
 
   return (
     <div className="p-32 h-page flex justify-center items-center">
       <div>
-        {token && <p>you are logged in</p>}
-        <h2>Login</h2>
-        <div className=" flex gap-2 items-center">
-          <input className="border p-1 border-amber-500" type="password" value={pin} onChange={(e) => setPin(e.target.value)} />
-          <button className="border p-1 border-amber-500" onClick={handleLogin}>
-            Log In
-          </button>
+        {/* {token && <p>you are logged in</p>} */}
+        <h2 className="text-center mb-6">Login</h2>
+
+        <div className="flex gap-2 items-center">
+          {pin.map((d, i) => (
+            <input key={i} maxLength="1" type="password" className="border w-12 h-12 text-center text-lg" value={d} ref={(ref) => (inputRefs.current[i] = ref)} onChange={(e) => handleChange(i, e.target.value)} onKeyDown={(e) => handleKeyDown(i, e)} />
+          ))}
         </div>
-        {error && <p>{error}</p>}
+        <p>{pin.join("")}</p>
+        {/* {error && <p>{error}</p>} */}
       </div>
     </div>
   );
