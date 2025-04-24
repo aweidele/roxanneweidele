2dd87ad11f57f55a7f8f46e91835199bc2210259
Aaron Weidele <aweidele@gmail.com>
2025-04-24 14:10:02 -0400

    environment?

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./functions/router";
 import { AppProvider } from "./components/AppContext";
+const basename = import.meta.env.VITE_BASENAME || "/";
 
 function App() {
   return (
-    <AppProvider>
-      <RouterProvider router={router} />
-    </AppProvider>
+    <>
+      <h2>{basename}</h2>
+      {/* <AppProvider>
+        <RouterProvider router={router} />
+      </AppProvider> */}
+    </>
   );
 }
 
