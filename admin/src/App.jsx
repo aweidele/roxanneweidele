de08add3aa88c11d47b9067f7223b7dc446f758c
Aaron Weidele <aweidele@gmail.com>
2025-04-24 14:14:10 -0400

    basename

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,16 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./functions/router";
 import { AppProvider } from "./components/AppContext";
-const basename = import.meta.env.VITE_BASENAME || "/";
 
 function App() {
   return (
-    <>
-      <h2>{basename}</h2>
-      {/* <AppProvider>
-        <RouterProvider router={router} />
-      </AppProvider> */}
-    </>
+    <AppProvider>
+      <RouterProvider router={router} />
+    </AppProvider>
   );
 }
 
--- admin/src/functions/router.jsx
+++ admin/src/functions/router.jsx
@@ -2,15 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import { AuthGate } from "../components/AuthGate";
 import { HomePage } from "../pages/HomePage";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AuthGate />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-    ],
-  },
-]);
+const basename = import.meta.env.VITE_BASENAME || "/";
+
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AuthGate />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+      ],
+    },
+  ],
+  { basename }
+);
