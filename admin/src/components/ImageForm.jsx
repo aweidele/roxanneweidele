38a42c5e3dcb71c16b88663b80af1897c643943f
Aaron Weidele <aweidele@gmail.com>
2025-05-07 14:57:37 -0400

    image form

--- admin/src/components/ImageForm.jsx
+++ admin/src/components/ImageForm.jsx
@@ -1,3 +1,20 @@
+import { Input } from "./elements/Input";
+
 export const ImageForm = ({ artworkId }) => {
-  return <form></form>;
+  return (
+    <form>
+      <div className="flex gap-6">
+        <div className="flex-1">
+          <Input name="title" label="Title" required={true} outerClass="mb-1" />
+          <div className="flex gap-2">
+            <Input name="width" label="Width" type="number" required={true} outerClass="flex-1" />
+            <Input name="height" label="Height" type="number" required={true} outerClass="flex-1" />
+          </div>
+        </div>
+        <div className="flex-1">
+          <Input type="textarea" label="Description" outerClass="flex flex-col h-full" innerClass="grow-1" />
+        </div>
+      </div>
+    </form>
+  );
 };
--- admin/src/components/elements/Input.jsx
+++ admin/src/components/elements/Input.jsx
@@ -1,3 +1,11 @@
-export const Input = () => {
-  return <input />;
+export const Input = ({ label, name, type, required, outerClass, innerClass }) => {
+  const Tag = type === "textarea" ? "textarea" : "input";
+  return (
+    <>
+      <label className={["block", outerClass].join(" ")}>
+        <span className="text-xs tracking-widest block mb-1">{label}</span>
+        <Tag type={type} className={`block border border-china-rose text-sm px-2 py-1 w-full ${innerClass}`} name={name} placeholder={label} required={required}></Tag>
+      </label>
+    </>
+  );
 };
