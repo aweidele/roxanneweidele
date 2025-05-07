2dbb1e8c9814fe94d2845a39b83ed9497ed585a7
Aaron Weidele <aweidele@gmail.com>
2025-05-07 15:34:47 -0400

    more form styling

--- admin/src/components/ImageUploads.jsx
+++ admin/src/components/ImageUploads.jsx
@@ -8,7 +8,7 @@ export const ImageUploads = () => {
     <>
       <h2>Uploaded Images</h2>
       {files.map((file) => (
-        <div key={`${file.name}-${Math.random()}`} className="w-full my-2 p-2 border border-rose-quartz flex gap-2">
+        <div key={`${file.name}-${Math.random()}`} className="w-full mt-2 mb-9 p-4 border border-rose-quartz flex gap-2">
           <div className="w-50 aspect-[533/300] shrink-0">
             <img src={file.loading ? file.preview : file.urls.thumb.jpg} alt={file.name} className={`w-full h-full object-cover ${file.loading ? " opacity-50" : ""} transition-all duration-500`} />
           </div>
--- admin/src/components/elements/Button.jsx
+++ admin/src/components/elements/Button.jsx
@@ -1,3 +1,3 @@
 export const Button = ({ children }) => {
-  return <button className="bg-china-rose text-white uppercase font-heading tracking-wide py-2 px-2">{children}</button>;
+  return <button className="cursor-pointer bg-china-rose hover:bg-rose-quartz text-white uppercase font-heading tracking-wide py-2 px-2 rounded-lg shadow shadow-slate-300">{children}</button>;
 };
