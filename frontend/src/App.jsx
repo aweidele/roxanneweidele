b3c9e73b5b31cc486f4c32c3b9b08aa57bc0275c
Aaron Weidele <aweidele@gmail.com>
2025-06-03 16:20:24 -0400

    testing shared components on front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
-import { apiURL } from "@shared";
+import { useEffect, useState } from "react";
+import { apiURL, useApi } from "@shared";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [gallery, setGallery] = useState([]);
+  const { request } = useApi();
+  console.log(gallery);
+
+  useEffect(() => {
+    const getGallery = async () => {
+      const result = await request();
+      setGallery(result.artwork.filter((item) => item.published));
+    };
+    getGallery();
+  }, []);
 
   return (
     <>
       <div>hi</div>
       <div>{apiURL}</div>
+      <ul>
+        {gallery.map((item) => (
+          <li>{item.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
