f690e12ed575c6c740e86aa3697ed91161b8df17
Aaron Weidele <aweidele@gmail.com>
2025-06-09 23:14:44 -0400

    Active state on nav

--- frontend/src/components/layout/header/Nav.jsx
+++ frontend/src/components/layout/header/Nav.jsx
@@ -1,13 +1,15 @@
 import { NavLink } from "react-router-dom";
 import { navigation } from "../../../functions/content";
 
+const navClasses = `text-white hover:text-uranian-blue tracking-wide inline-block relative after:content-[""] after:w-full after:bg-uranian-blue after:absolute after:left-0 after:top-full after:transition-all after:duration-300`;
+
 export const Nav = () => {
   return (
     <nav>
       <ul className="flex gap-5">
         {navigation.map((item) => (
           <li key={item.to} className="text-button uppercase">
-            <NavLink to={item.to} className="text-white hover:text-uranian-blue tracking-wide">
+            <NavLink to={item.to} className={({ isActive }) => `${navClasses} ${isActive ? "after:h-1" : "after:h-0 after:opacity-0"}`}>
               {item.label}
             </NavLink>
           </li>
