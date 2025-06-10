f0f72662605ee640e2a8efcf372391e54cb7c657
Aaron Weidele <aweidele@gmail.com>
2025-06-10 00:05:15 -0400

    Mobile Nav button

--- frontend/src/components/layout/header/Nav.jsx
+++ frontend/src/components/layout/header/Nav.jsx
@@ -3,13 +3,13 @@ import { navigation } from "../../../functions/content";
 
 const navClasses = `text-white hover:text-uranian-blue tracking-wide inline-block relative after:content-[""] after:w-full after:bg-uranian-blue after:absolute after:left-0 after:top-full after:transition-all after:duration-300`;
 
-export const Nav = ({ ...props }) => {
+export const Nav = ({ handleClose, ...props }) => {
   return (
     <nav {...props}>
-      <ul className="flex gap-5 max-md:flex-col max-md:w-full text-center">
+      <ul className="flex gap-8 md:gap-5 max-md:flex-col max-md:w-full text-center">
         {navigation.map((item) => (
           <li key={item.to} className="text-xl md:text-button uppercase">
-            <NavLink to={item.to} className={({ isActive }) => `${navClasses} ${isActive ? "after:h-1" : "after:h-0 after:opacity-0"}`}>
+            <NavLink to={item.to} className={({ isActive }) => `${navClasses} ${isActive ? "after:h-1" : "after:h-0 after:opacity-0"}`} onClick={handleClose}>
               {item.label}
             </NavLink>
           </li>
