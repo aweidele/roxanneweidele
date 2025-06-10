291ddfd42673febbae87cc385f5b4426204a3529
Aaron Weidele <aweidele@gmail.com>
2025-06-09 23:29:55 -0400

    Mobile Nav

--- frontend/src/components/layout/header/Header.jsx
+++ frontend/src/components/layout/header/Header.jsx
@@ -21,12 +21,14 @@ export const Header = ({ ...props }) => {
         <h1 className="sr-only">Portfolio of Roxanne Weidele, Maryland-based artist specializing in calm, natural landscape paintings in bold, lush colors.</h1>
         <Container w={scrolled ? "full" : "wide"} className={`py-4${scrolled ? "" : " md:py-11"} transition-all duration-500`}>
           <div className="flex justify-between items-center gap-5">
-            <NavLink to="/" className="text-uranian-blue hover:text-white">
+            <NavLink to="/" className="text-uranian-blue hover:text-white max-md:relative max-md:z-40">
               <Logo className="w-28 lg:w-56.25 fill-current" />
               <span className="sr-only">Home</span>
             </NavLink>
-            <Nav />
-            <Social />
+            <div className="fixed top-0 left-0 px-5 py-10 w-screen h-screen bg-uranian-blue-900-80 z-30 flex flex-col items-stretch backdrop-blur-sm md:contents">
+              <Nav className="max-md:grow-1 max-md:flex max-md:items-center" />
+              <Social />
+            </div>
           </div>
         </Container>
       </header>
--- frontend/src/components/layout/header/Nav.jsx
+++ frontend/src/components/layout/header/Nav.jsx
@@ -3,12 +3,12 @@ import { navigation } from "../../../functions/content";
 
 const navClasses = `text-white hover:text-uranian-blue tracking-wide inline-block relative after:content-[""] after:w-full after:bg-uranian-blue after:absolute after:left-0 after:top-full after:transition-all after:duration-300`;
 
-export const Nav = () => {
+export const Nav = ({ ...props }) => {
   return (
-    <nav>
-      <ul className="flex gap-5">
+    <nav {...props}>
+      <ul className="flex gap-5 max-md:flex-col max-md:w-full text-center">
         {navigation.map((item) => (
-          <li key={item.to} className="text-button uppercase">
+          <li key={item.to} className="text-xl md:text-button uppercase">
             <NavLink to={item.to} className={({ isActive }) => `${navClasses} ${isActive ? "after:h-1" : "after:h-0 after:opacity-0"}`}>
               {item.label}
             </NavLink>
