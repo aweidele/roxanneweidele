b2e517f0d560009fcb5197e23d105308c766c57a
Aaron Weidele <aweidele@gmail.com>
2025-06-12 07:26:32 -0400

    header

--- frontend/src/components/layout/header/Social.jsx
+++ frontend/src/components/layout/header/Social.jsx
@@ -2,7 +2,7 @@ import { Facebook, Instagram } from "../Icons";
 
 export const Social = ({ handleClose }) => {
   return (
-    <ul className="flex justify-center md:flex-col gap-3.5">
+    <ul className="flex justify-center md:flex-col gap-3.5 justify-self-end">
       <li>
         <a href="https://www.facebook.com/roxanne.weidele.artist/" target="_blank" className="text-white hover:text-uranian-blue" onClick={handleClose}>
           <Facebook className="w-6.5 h-6.5 fill-current" />
