b4c6c85c5e93a9ae339c339f001707033f5ad66a
Aaron Weidele <aweidele@gmail.com>
2025-07-01 10:42:57 -0400

    remove test suffix from image url

--- packages/shared/functions/functions.js
+++ packages/shared/functions/functions.js
@@ -61,4 +61,4 @@ export const stringToLines = (str) => {
     .filter((line) => line);
 };
 
-export const imageURL = (image) => `${apiURL}${image.upload_path_rel}${image.filename}?hi=hello`;
+export const imageURL = (image) => `${apiURL}${image.upload_path_rel}${image.filename}`;
