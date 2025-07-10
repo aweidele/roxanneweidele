c058ecead34f594fef2981b704a0f1475bc54d51
Aaron Weidele <aweidele@gmail.com>
2025-07-10 10:11:00 -0400

    fix: routes do not work when entered manually

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,21 +4,53 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "",
-  resolve: {
-    alias: {
-      "@shared": path.resolve(__dirname, "../packages/shared"),
-      "@functions": path.resolve(__dirname, "./src/functions"),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      css: {
-        // Needed for @import aliases to resolve properly
-        additionalData: "",
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: "",
+//   resolve: {
+//     alias: {
+//       "@shared": path.resolve(__dirname, "../packages/shared"),
+//       "@functions": path.resolve(__dirname, "./src/functions"),
+//     },
+//   },
+//   css: {
+//     preprocessorOptions: {
+//       css: {
+//         // Needed for @import aliases to resolve properly
+//         additionalData: "",
+//       },
+//     },
+//   },
+// });
+
+export default defineConfig(({ mode }) => {
+  console.log("---------------- MODE ----------------");
+  console.log(mode);
+  let base = "/";
+  if (mode === "development") {
+    base = "/"; // Local dev server
+  } else if (mode === "staging") {
+    base = "/admin/";
+  } else if (mode === "admin-prod") {
+    base = "/"; // Hosted on admin.xyz.com
+  }
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base,
+    resolve: {
+      alias: {
+        "@shared": path.resolve(__dirname, "../packages/shared"),
+        "@functions": path.resolve(__dirname, "./src/functions"),
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        css: {
+          // Needed for @import aliases to resolve properly
+          additionalData: "",
+        },
+      },
+    },
+  };
 });
