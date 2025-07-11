7836c9d34b811c29739dd06eea9c79f280d44d3c
Aaron Weidele <aweidele@gmail.com>
2025-07-10 22:33:28 -0400

    Handling scrolling with title in center

--- frontend/src/components/layout/Template.jsx
+++ frontend/src/components/layout/Template.jsx
@@ -8,7 +8,6 @@ export const Template = ({ hasSlug = false, children }) => {
       <Header className={`w-full fixed top-0 left-0 z-50 transition-all duration-300 ${hasSlug ? "md:opacity-50 md:hover:opacity-100" : ""}`} hasSlug={hasSlug} />
       <main>{children}</main>
       <Footer />
-      <Colors />
     </>
   );
 };
--- frontend/src/components/layout/header/Header.jsx
+++ frontend/src/components/layout/header/Header.jsx
@@ -1,20 +1,25 @@
 import { Container, Logo } from "../index";
 import { NavLink } from "react-router-dom";
 import { Social } from "./Social";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Nav } from "./Nav";
 
 export const Header = ({ hasSlug = false, ...props }) => {
   const [scrolled, setScrolled] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [headingMT, setHeadingMT] = useState(0);
 
   const navLines = "block h-0.5 bg-white absolute left-0 top-[calc(50%-0.25rem)] transition-all duration-500";
   const line1Class = `${navLines} ${menuOpen ? "w-4/5 rotate-45" : "w-full -translate-y-1.25"}`;
   const line2Class = `${navLines} ${menuOpen ? "w-4/5 -rotate-45" : "w-3/4 translate-y-1.25"}`;
 
+  const headingRef = useRef();
+
   useEffect(() => {
     const handleScroll = () => {
       setScrolled(window.scrollY > 100);
+      const headingHeight = headingRef.current.offsetHeight;
+      setHeadingMT(window.scrollY > 100 ? `-${headingHeight}px` : 0);
     };
     window.addEventListener("scroll", handleScroll);
 
@@ -31,7 +36,7 @@ export const Header = ({ hasSlug = false, ...props }) => {
         {/* <h1 className="sr-only">Portfolio of Roxanne Weidele, Maryland-based artist specializing in calm, natural landscape paintings in bold, lush colors.</h1> */}
         <Container w="full" className={`py-4 transition-all duration-500`}>
           <div className="max-md:flex justify-between items-center gap-5 relative">
-            <h1 className="md:text-center">
+            <h1 className={`md:text-center transition-300 ${scrolled ? "md:opacity-0" : ""}`} ref={headingRef} style={{ marginTop: headingMT }}>
               <NavLink to="/" className="text-white hover:text-uranian-blue max-md:relative max-md:z-40 text-xl-sm md:text-2xl uppercase tracking-widest" onClick={() => setMenuOpen(false)}>
                 Roxanne Weidele
               </NavLink>
@@ -42,7 +47,7 @@ export const Header = ({ hasSlug = false, ...props }) => {
               <span className="sr-only">Menu</span>
             </button>
             <div className={`menu ${menuOpen ? "" : " max-md:translate-x-full"}`}>
-              <Nav className={`max-md:grow-1 max-md:flex max-md:items-center md:py-2 md:px-6  transition-all duration-500${scrolled ? " md:bg-uranian-blue-900-80 md:rounded-2xl" : ""}`} handleClose={() => setMenuOpen(false)} />
+              <Nav className={`max-md:grow-1 flex items-center justify-center md:py-2 md:px-6`} handleClose={() => setMenuOpen(false)} scrolled={scrolled} />
               <Social handleClose={() => setMenuOpen(false)} />
             </div>
           </div>
