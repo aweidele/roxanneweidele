e5acbdd4d5ac1f42a72bb2444a4c6c4b235158bf
Aaron Weidele <aweidele@gmail.com>
2025-07-10 23:48:51 -0400

    adding unpublished dropzone

--- admin/src/components/Gallery.jsx
+++ admin/src/components/Gallery.jsx
@@ -2,6 +2,8 @@ import { Section } from "./Section";
 import { useGalleryContext } from "./GalleryContext";
 import { GalleryGrid } from "./GalleryGrid";
 import { useState } from "react";
+import { IconUnpublished } from "./elements/Icons";
+import { Button } from "@shared/components/Button";
 
 export const Gallery = () => {
   const { gallery } = useGalleryContext();
@@ -9,16 +11,23 @@ export const Gallery = () => {
   const unpublished = gallery.filter((item) => !item.published);
 
   const [leftColumn, setLeftColumn] = useState(published);
-  const [rightColumn, setRightColumn] = useState(`${unpublished.length} Unpublished items`);
 
   return (
     <Section>
       <h2 className="mb-8">Gallery Grid</h2>
-      <div className="flex gap-5">
+      <div className="flex gap-15">
         <div className="flex-3/4">
           <GalleryGrid content={leftColumn} />
         </div>
-        <div className="flex-1/4">{rightColumn}</div>
+        <div className="flex-1/4">
+          <div className="sticky top-0">
+            <h3 className="mb-4 leading-[2.5rem]">Unpublished Items ({unpublished.length})</h3>
+            <div className="aspect-5/4 border-2 border-dashed rounded-xl border-gray-300 flex flex-col justify-center items-center text-center p-10">
+              <IconUnpublished className="fill-cordovan w-18 h-18 block mx-auto mt-6 transition-300" />
+              <p>Drag images from the gallery to set to unpublished</p>
+            </div>
+          </div>
+        </div>
       </div>
     </Section>
   );
--- admin/src/components/GalleryGridCard.jsx
+++ admin/src/components/GalleryGridCard.jsx
@@ -14,11 +14,11 @@ export const GalleryGridCard = ({ item, active }) => {
           <img src={imageURL(item.files.lg)} className="w-full h-full object-contain" />
           {!!item.sold && <div className="bg-cordovan rounded-full w-7 h-7 text-white text-xxs uppercase flex items-center justify-center -rotate-45 absolute bottom-2 right-2 shadow-sold">Sold</div>}
         </div>
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between">
           <div className="p-1">
             <h3>{item.title}</h3>
           </div>
-          <NavLink to={`/artwork/${item.slug}`} className="cursor-pointer text-cordovan hover:text-black">
+          <NavLink to={`/artwork/${item.slug}`} className="block cursor-pointer text-cordovan hover:text-black py-3">
             <IconEdit className="w-3 h-3 fill-current" />
           </NavLink>
         </div>
