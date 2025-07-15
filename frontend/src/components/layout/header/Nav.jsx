d2c3e1a170e0a4d6cb80637494f063d2cb84da88
Aaron Weidele <aweidele@gmail.com>
2025-07-15 00:27:16 -0400

    Filtering using React Router

--- frontend/src/components/layout/header/Nav.jsx
+++ frontend/src/components/layout/header/Nav.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { navigation } from "../../../functions/content";
+import { navigation } from "@functions/content";
 
 const navClasses = `text-white hover:text-uranian-blue tracking-wide inline-block relative after:content-[""] after:w-full after:bg-uranian-blue after:absolute after:left-0 after:top-full after:transition-all after:duration-300`;
 
