bcf8e39854226da165cb78c4a27c54da58c88176
Aaron Weidele <aweidele@gmail.com>
2025-07-24 11:22:15 -0400

    homepage links

--- frontend/src/components/elements/ArtworkLink.jsx
+++ frontend/src/components/elements/ArtworkLink.jsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom";
 
 export const ArtworkLink = ({ img, link, label }) => {
   return (
-    <NavLink to={link} className="text-white hover:text-uranian-blue no-underline">
-      <img src={img} className="block border-10 border-current w-full" />
+    <NavLink to={link} className="text-white hover:text-uranian-blue no-underline group aspect-[1800/1243] overflow-hidden border-10 border-current block rounded-sm">
+      <img src={img} className="block w-full h-full object-cover group-hover:scale-110 transition-300" />
       <p className="text-center my-2 uppercase font-heading text-xl tracking-wide">{label}</p>
     </NavLink>
   );
